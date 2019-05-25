ea141575ae067a4aa890f04ea39200592593ef4a
JUshine <JUshine1209@gmail.com>
2019-05-25 16:12:20 +0900

    listitem fix

--- app/src/main/java/edu/skku/sw3/success/ListItem.java
+++ app/src/main/java/edu/skku/sw3/success/ListItem.java
@@ -1,4 +1,21 @@
 package edu.skku.sw3.success;
 
 public class ListItem {
+    private String Title;
+    private String Date;
+    private String Category;
+
+    public ListItem() {}
+
+    public ListItem(String Title, String Date, String Category){
+        this.Title = Title;
+        this.Date = Date;
+        this.Category = Category;
+    }
+
+    public String getTitle() {return Title;}
+
+    public String getDate() {return Date;}
+
+    public String getCategory() {return  Category;}
 }
